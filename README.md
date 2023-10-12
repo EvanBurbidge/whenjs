480d62d1a9c356fce1ba643bc3912597b8292d60
Evan Burbidge <burbidge@tcd.ie>
2023-10-12 22:26:13 +0100

    update packages

---
+++ README.md
@@ -0,0 +1,71 @@
+# When.js
+
+This is a small implementation to mock the `when` functionality that is provided by kotlin. In this library, if a matching case is found, only the code in the respective case block is executed, and execution continues with the next statement after the when block.
+
+This essentially means that we don’t need break statements at the end of each case block.
+
+## Installation
+
+`npm install whenjs`
+
+## Usage
+
+### String matching
+```javascript
+when('11', {
+  "12": () => "not eleven?",
+  '11': () => "Bingo! It's eleven."
+}); // outputs "Bingo! It's eleven."
+```
+
+### Number matching
+```javascript
+when(12, {
+  "cat": () => "Cat really?",
+  12: (identifier) => String(identifier),
+}); // ouputs "12"
+
+```
+
+### Multiple conditions on one line
+If we want to run the same function for multiple conditions then we are able to do so by passing a comma separated string of the possible values this is similar to stacking cases in switch.
+
+```javascript
+when('2', {
+  '11': () => "Bingo! It's eleven.",
+  "1,2,3": () => "Multi condition value",
+}); // outputs "Multi condition value"
+```
+
+### Default cases
+Sometimes we may not find the value and have to fallback on an else value to use this feature pass an else keyword and a function.
+```javascript
+when('three', {
+  '11': () => "Bingo! It's eleven.",
+  else: () => 'Else value',
+});
+```
+
+### Nested whens 
+Sure why not
+```javascript
+
+when('11', {
+  '11': (identifier) => when(identifier, {
+    '11': () => 'Nested'
+  }),
+})
+
+```
+
+### Checking with conditions
+In order to check boolean conditions we need to first omit the condition value and pass our object as the first argument, then we need to pass true as the second value in order to enable conditional comparisons. We must wrap each key with square brackets and wrap the conditions in a `String()` declaration to convert it to be usable in our code
+
+```javascript
+when({
+  [String('11' === '11')]: () => "true value",
+  [String('12' === '11')]: () => "false value",
+}, true)
+
+```
+
---
+++ When.test.js
@@ -0,0 +1,74 @@
+const when = require("./whenjs");
+
+describe("When.js", () => {
+  it("should get 'bingo its 11'", () => {
+    const result = when('11', {
+      "12": () => "not eleven?",
+      '11': () => "Bingo! It's eleven."
+    });
+    expect(result).toEqual("Bingo! It's eleven.");
+  });
+
+  it("should get 12", () => {
+    const result = when(12, {
+      "cat": () => "Cat really?",
+      12: (identifier) => String(identifier),
+    });
+    expect(result).toEqual('12');
+  });
+
+  it("should get get the else value", () => {
+    const result = when('13', {
+      '11': () => "Bingo! It's eleven.",
+      else: () => 'Else value'
+    });
+    expect(result).toEqual("Else value");
+  });
+
+  it("should be able to check 'array values'", () => {
+    const result = when('2', {
+      '11': () => "Bingo! It's eleven.",
+      "1,2,3": () => "Array value",
+    });
+    expect(result).toEqual("Array value");
+  });
+
+  it("should be able to check 'array values with more complex strings'", () => {
+    const result = when('three', {
+      "cat": () => "Cat really?",
+      12: () => "Close but not close enough.",
+      '11': () => "Bingo! It's eleven.",
+      else: () => 'Else value',
+      "one,two,three": () => "Array value",
+    });
+    expect(result).toEqual("Array value");
+  });
+
+  it("should handle the case where no operator matches", () => {
+    const result = when('15', {
+      "cat": () => "Cat really?",
+      12: () => "Close but not close enough.",
+      '11': () => "Bingo! It's eleven.",
+      "one,two,three": () => "Array value",
+    });
+    expect(result).toEqual("No matches");
+  });
+
+  it("can use a nested when", () => {
+    const result = when('11', {
+      '11': (identifier) => when(identifier, {
+        '11': () => 'Nested'
+      }),
+    });
+    expect(result).toEqual("Nested");
+  });
+
+  it('should check via conditionals', () => {
+    const result = when({
+      [String('11' === '11')]: () => "true value",
+      [String('12' === '11')]: () => "false value",
+    }, true);
+    expect(result).toEqual('true value');
+  })
+
+});

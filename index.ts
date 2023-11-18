1fac43334f62d99ea6e3823a90871813162b22eb
Evan Burbidge <burbidge@tcd.ie>
2023-11-18 21:01:41 +0000

    update when to return operator if its found or loop

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 export function when<T>(...args: any): T | string {
   let condition: string = '';
-  let conditions: Record<string, (e?: any) => T> = {};
   let useConditional: boolean = false;
+  let conditions: Record<string, (e?: any) => T> = {};
 
   if (args.length === 2 && typeof args[1] !== 'boolean') {
     condition = args[0];
@@ -43,9 +43,6 @@ export function when<T>(...args: any): T | string {
     if (conditions.hasOwnProperty('else')) {
       return conditions.else(condition);
     }
-  } else if (operator) {
-    return operator(condition);
   }
-
   return "No matches"
 }
